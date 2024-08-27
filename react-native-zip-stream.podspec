5ddf2186f849067700f90d03d13d95bafdd5c7a1
Alexandre <alexandre@MacBook-Air-de-Alexandre.local>
2024-08-27 13:21:54 +0200

    scripts

--- react-native-zip-stream.podspec
+++ react-native-zip-stream.podspec
@@ -1,8 +1,11 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
+
 Pod::Spec.new do |s|
-  s.name         = 'react-native-zip-stream'
-  s.version      = '1.0.0'
-  s.summary      = 'A React Native module for streaming ZIP files.'
-  s.description  = 'This module allows you to list and stream files from a ZIP archive in a React Native application.'
+  s.name         = package['name']
+  s.version      = package['version']
+  s.summary      = package['description']
   s.homepage     = 'https://github.com/agarrec-vivlio/react-native-zip-stream'
   s.license      = { :type => 'MIT', :file => 'LICENSE' }
   s.author       = { 'Your Name' => 'your-email@example.com' }
---
+++ scripts/tag_npm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if there are uncommitted changes
+if [[ -n $(git status --porcelain) ]]; then
+  echo "There are uncommitted changes. Please commit or stash them before proceeding."
+  exit 1
+fi
+
+# Bump the package version
+echo "Choose the version type you want to increment (patch, minor, major):"
+read version_type
+
+yarn version --$version_type || exit 1
+
+# Get the new version
+new_version=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | tr -d '", ')
+
+# Add changes and commit
+git add package.json yarn.lock
+git commit -m "Bump version to $new_version"
+
+# Push to GitHub
+git push origin main
+
+# Create a tag with the new version
+git tag "v$new_version"
+git push origin "v$new_version"
+
+echo "Version $new_version successfully created and tagged on GitHub."
