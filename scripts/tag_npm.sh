e7e8c6f71edbc21cac8d7f11aaba37f85657acd0
Alexandre <alexandre@MacBook-Air-de-Alexandre.local>
2024-09-02 14:45:05 +0200

    chore: test

--- scripts/tag_npm.sh
+++ scripts/tag_npm.sh
@@ -6,14 +6,24 @@ if [[ -n $(git status --porcelain) ]]; then
   exit 1
 fi
 
-# Bump the package version
-echo "Choose the version type you want to increment (patch, minor, major):"
-read version_type
+# Prompt for version type and validate input
+valid_version_types=("patch" "minor" "major")
+while true; do
+  echo "Choose the version type you want to increment (patch, minor, major):"
+  read version_type
 
+  if [[ " ${valid_version_types[@]} " =~ " ${version_type} " ]]; then
+    break
+  else
+    echo "Invalid version type. Please choose from patch, minor, or major."
+  fi
+done
+
+# Bump the package version
 yarn version --$version_type || exit 1
 
 # Get the new version
-new_version=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | tr -d '", ')
+new_version=$(grep -oP '(?<="version": ")[^"]*' package.json)
 
 # Add changes and commit
 git add package.json yarn.lock
@@ -28,4 +38,4 @@ git push origin "v$new_version"
 
 npm publish
 
-echo "Version $new_version successfully created and tagged on GitHub."
+echo "Version $new_version successfully created, tagged on GitHub, and published to npm."
